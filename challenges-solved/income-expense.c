5d1439ccdc3d015978743c6a1bd3ed6185ac86be
dottydaily <dottypurkt@gmail.com>
2017-03-06 04:35:01 +0700

    Update income-expense.c

--- challenges-solved/income-expense.c
+++ challenges-solved/income-expense.c
@@ -71,69 +71,72 @@ int main()
 		printf("\nIncome :\n");
 		for (day = 6 ; day <= 30 ; day+= 6){
 			printf(">> Day %2d : ", day);
-			scanf("%f", income[day]);
+			scanf("%f", &income[day]);
 
 			if (income[day] <= 1){
 				logic = 0;
+				printf("\nERROR");
 				break;
 			}
 
 			sum_income += income[day];
 		}
 
-		//Input Expense
-		printf("\nExpense :\n");
+		if (logic){
+			//Input Expense
+			printf("\nExpense :\n");
 
-		do{	
-			printf(">> Day : ");
-			scanf("%d", day);
+			do{	
+				printf(">> Day : ");
+				scanf("%d", &day);
 
-			if(day > 0 || day < 31){
-				printf("= ");
-				scanf("%f", expense[day]);
-				
-				if (expense[day] < 1){
-					logic = 0;
-					break;
+				if(day > 0 || day < 31){
+					printf("= ");
+					scanf("%f", &expense[day]);
+					
+					if (expense[day] < 1){
+						logic = 0;
+						break;
+					}
+
+					sum_expense += expense[day];
+					cnt_day++;
 				}
+				else
+					printf("***** End *****\n\n");
+			} while (day > 0 || day < 31) ;
 
-				sum_expense += expense[day];
-				cnt_day++;
+			if (logic == 0)
+				printf("\nERROR");
+			else{
+				//Find max, min, avg and print it.
+				find_max(income, max);
+				printf("Max income : %.2f Baht. %2.0f/%d/%d", max[0], max[1], month, year);
+				
+				find_min(income, max);
+				printf("Min income : %.2f Baht. %2.0f/%d/%d", min[0], min[1], month, year);
+
+				find_max(expense, max);
+				printf("Max expense : %.2f Baht.", max[0]);
+				if(sum_expense!=0)
+					printf(" %2.0f/%d/%d\n",  max[1], month, year);
+
+				find_min(expense, max);
+				printf("Min expense : %.2f Baht.", min[0]);
+				if(sum_expense!=0)
+					printf(" %2.0f/%d/%d\n",  min[1], month, year);
+				
+				avg_income = sum_income/5.0;
+				avg_expense = sum_expense/cnt_day;
+				printf("Avg income : %.2f Baht.\n", avg_income);
+				printf("Avg expense : %.2f Baht.\n", avg_expense);
+
+				if (avg_income != avg_expense)
+					printf("You have average %s more than average %s.", 
+						(avg_income>avg_expense?/*"income":"expense"));*/("income","expense"):("expense","income")));
+				else
+					printf("You have average income equal to average expense.");
 			}
-			else
-				printf("***** End *****\n\n");
-		} while (day > 0 || day < 31) ;
-
-		if (logic == 0)
-			printf("\nERROR");
-		else{
-			//Find max, min, avg and print it.
-			find_max(income, max);
-			printf("Max income : %.2f Baht. %2.0f/%d/%d", max[0], max[1], month, year);
-			
-			find_min(income, max);
-			printf("Min income : %.2f Baht. %2.0f/%d/%d", min[0], min[1], month, year);
-
-			find_max(expense, max);
-			printf("Max expense : %.2f Baht.", max[0]);
-			if(sum_expense!=0)
-				printf(" %2.0f/%d/%d\n",  max[1], month, year);
-
-			find_min(expense, max);
-			printf("Min expense : %.2f Baht.", min[0]);
-			if(sum_expense!=0)
-				printf(" %2.0f/%d/%d\n",  min[1], month, year);
-			
-			avg_income = sum_income/5.0;
-			avg_expense = sum_expense/cnt_day;
-			printf("Avg income : %.2f Baht.\n", avg_income);
-			printf("Avg expense : %.2f Baht.\n", avg_expense);
-
-			if (avg_income != avg_expense)
-				printf("You have average %s more than average %s.", 
-					(avg_income>avg_expense?/*"income":"expense"));*/("income","expense"):("expense","income")));
-			else
-				printf("You have average income equal to average expense.");
 		}
-	}
+	}	
 }
